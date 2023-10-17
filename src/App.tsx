a0367fef545fdf88659413495f383e358a9f8939
Pablo Cuevas <cuevasgpablo@gmail.com>
2023-10-17 14:58:04 -0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Navigation } from './router/navigation';
+
+function App() {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+}
+
+export default App;
---
+++ src/router/navigation.tsx
@@ -0,0 +1,57 @@
+import {
+  BrowserRouter,
+  NavLink,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
+
+export const Navigation = () => {
+  return (
+    <BrowserRouter>
+      <div className="main-layout">
+        <nav>
+          <section>
+            <img src="src/assets/react.svg" alt="Vite Logo" height={80} />
+            <span>+</span>
+            <img src="/vite.svg" alt="Vite Logo" height={80} />
+          </section>
+
+          <ul>
+            <li>
+              <NavLink
+                to="/home"
+                className={({ isActive }) => (isActive ? 'nav-active' : '')}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                className={({ isActive }) => (isActive ? 'nav-active' : '')}
+              >
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/users"
+                className={({ isActive }) => (isActive ? 'nav-active' : '')}
+              >
+                Users
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="about" element={<h1>About Page</h1>} />
+          <Route path="users" element={<h1>Users Page</h1>} />
+          <Route path="home" element={<h1>Home Page</h1>} />
+          <Route path="/*" element={<Navigate to="/home" replace />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
